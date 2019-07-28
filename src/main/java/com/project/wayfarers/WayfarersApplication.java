939fe42bc20ef9e5dfb540cae2782c11c0326ed6
vishakha <vishakhaj.18@gmail.com>
2019-07-28 17:41:16 +0200

    source project

---
+++ src/main/java/com/project/wayfarers/WayfarersApplication.java
@@ -0,0 +1,13 @@
+package com.project.wayfarers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WayfarersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WayfarersApplication.class, args);
+	}
+
+}
